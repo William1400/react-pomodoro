8ec1434ae1e6107f4b0121c352edd01e9ab43067
unknown <william.bady.1400@gmail.com>
2022-01-09 20:18:45 +0100

    create a time with Parcel

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Pomodoro from "./Pomodoro"
+
+function App() {
+
+    return (
+
+        <div>
+            <Pomodoro />
+        </div>
+    );
+}
+
+export default App;
